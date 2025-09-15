b27af0b6c5e873108d3578b8519ca1f50fc9a21b
Mandus Lindström <mandus85@hotmail.com>
2025-09-15 14:53:05 +0200

    done with code along timne for exercises

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// console.log("hej på dig!");
+
+const firstName = "Mandus";
+const age = 40;
+
+// let firstNameAndAge = firstName + age;
+
+// console.log(typeof firstName + typeof age);
+// console.log(typeof firstNameAndAge);
+
+// let fullName = "Mandus Lindström";
+
+// const lastName = "Lindström";
+
+// console.log(firstName);
+// console.log(lastName);
+
+// let fullName = firstName + " " + lastName;
+
+// Template literals (backtick)
+// let fullName = `Hej från ${firstName}${lastName} jag är gammal`;
+
+// console.log(fullName);
+
+// Promt
+let userName = prompt("Vad heter du?");
+
+let isTeacher = true;
+
+// console.log(userNamegustav);
+
+// typeof
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof userName);
+console.log(typeof isTeacher);
+
+let test = 1;
+
+test = 2;
+test = false;
+test = "Hej";
+
+console.log(typeof test);
